d8f2e93b16d9759b8a5657245867e47b68b4938d
Samuel Lopez <samuellopezdemiguel586@gmail.com>
2022-09-18 18:55:49 +0200

    Dia uno completado

---
+++ d001.py
@@ -0,0 +1,44 @@
+""" print("Hello, world!") """
+
+""" frase_bienvenida = "Primer dia de Python"
+print(frase_bienvenida) """
+
+""" variable_1 = "Hola"
+variable_2 = variable_1
+print(variable_2) """
+
+""" a = "este es mi valor actual"
+print(a)
+a = "mi valor a cambiado"
+print(a) """
+
+""" nombre = input("Por favor, introduce tu nombre:\n")
+print("Hola " + nombre + ".") """
+
+# Esto es un comentario # Hola 
+# Ejercicios:
+
+""" variable = "Estoy en el día 1 del reto de Python."
+print(variable)
+
+a = "rojo"
+b = "naranja"
+c = "azul"
+d = "verde"
+a = d
+d = b
+c = d
+print(a)
+print(c)
+
+frase_1 = "Hola"
+frase_2 = "Buenos dias."
+print(frase_1 + " " + frase_2) """
+
+# Proyecto:
+
+print("Hola, le voy a hacer una serie de preguntas para conocerle mejor")
+pregunta_1 = input("Cuantos años tienes? ")
+pregunta_2 = input("Cual es tu nombre? ")
+pregunta_3 = input("Donde a nacido? ")
+print("Hola " + pregunta_2 + " tienes: " + pregunta_1 + " años y naciste en " + pregunta_3)
