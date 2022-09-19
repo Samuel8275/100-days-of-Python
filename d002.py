788aeaf6ca16e1c1239d9885ecb9bca025c6bf9b
Samuel Lopez <samuellopezdemiguel586@gmail.com>
2022-09-19 16:12:24 +0200

    Dia dos completado

---
+++ d002.py
@@ -0,0 +1,103 @@
+"Esto es un string"
+'Esto tambien es un string'
+
+""" print('"Blue" es azul en ingles')
+
+color = "Amarillo"
+print(len(color))
+
+color = "Amarillo"
+print(color[3])
+"""
+
+""" esto_es_un_int = 100
+numero_2 = 200
+suma = esto_es_un_int + numero_2
+resta = esto_es_un_int - numero_2
+division = esto_es_un_int / numero_2
+multiplicacion = esto_es_un_int * numero_2
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division) """
+
+""" print(2**10)
+numero = 8_374_920_183_990_173
+print(numero) """
+
+""" float1 = 3.53
+integer = 100
+print(float1 * integer) """ 
+
+""" boolean1 = True
+boolean2 = False
+print(type(boolean1))
+print(type(boolean2)) """
+
+# La funcion len solo funciona con strings Pero podemos transformar el numero en string
+
+""" numero = 1283
+numero_str = str(numero)
+print(len(numero_str))
+
+string = "50.3"
+string_2 = "100"
+suma = float(string) + int(string_2)
+print(string + string_2)
+print(suma)
+ """
+
+""" multiplicacion = 54.8372 * 8.01939784609
+print(round(multiplicacion, 2)) """
+
+""" division = 10 // 3
+print(division)
+
+modulo = 10 % 3 
+print(modulo) """
+
+""" incremento = 10
+incremento += incremento;
+decremento = 10
+decremento -= incremento;
+print(incremento)
+print(decremento) """
+
+""" suma = 90 + 27
+print("El resultado de la suma es: " + str(suma))
+print(f"El resultado de la suma es: {suma}") """
+
+# Ejercicios
+
+""" variable = "automáticamente"
+exponente = 10**5
+exponente2 = 10 * 10 * 10 * 10 *10
+numero = 987862
+numero1 = 8.29374
+numero -= 1000
+print(len(variable))
+print(variable[5])
+print(exponente2)
+print(exponente)
+print(type(numero1))
+print(len(str(numero)))
+numero1 = "8.297348"
+numero1 = float(numero1)
+print(type(int(numero1)))
+print(round(numero1, 2))
+print(numero)
+numero += 100000
+print(numero)
+numero_1 = 4
+numero_2 = 769.83
+texto = "I'm a string?"
+decision = True
+print(f"El valor {numero_2} es bastante mas grande que {numero_1}. {texto} the anwser is {decision}") """
+
+# Proyecto 
+
+""" print("-- Calculadora de exponentes --")
+num1 = input("Introduzca un numero:\n")
+num2 = input("Introduzca otro numero:\n")
+resultado = int(num1)**int(num2)
+print(f"El resultado es: {resultado}") """
