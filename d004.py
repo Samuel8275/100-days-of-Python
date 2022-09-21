20aebeab121548e550474d920f827945fc1e508f
Samuel Lopez <samuellopezdemiguel586@gmail.com>
2022-09-21 11:06:04 +0200

    Dia quatro completado

---
+++ d004.py
@@ -0,0 +1,90 @@
+""" lista = ["azul", "rojo", "verde"]
+print(lista)
+print(lista[0])
+print(f"El segundo elemento de la lista es: {lista[1]}")
+print(lista[2][2])
+print(lista[-1])
+lista[1] = "amarillo"
+print(lista)
+lista.append("rojo")
+print(lista)
+lista.insert(2, "gris")
+print(lista)
+lista.clear()
+print(lista) """
+
+""" lista = ["azul", "rojo", "verde"]
+print(lista)
+lista.pop(2)
+print(lista)
+lista.append("verde")
+print(lista)
+lista.remove("verde")
+print(lista) """
+
+""" lista = ["azul", "rojo", "verde"]
+lista2 = lista.copy()
+print(lista2) """
+
+""" lista = ["azul", "rojo", "verde", "verde"]
+print(lista.count("verde"))
+print(lista.index("rojo"))
+lista.reverse()
+print(lista)
+lista.sort()
+print(lista)
+lista.sort(reverse=True)
+print(lista) """
+
+""" lista = [1, 4, 892, 2, 983, 35]
+lista.sort()
+print(lista) """
+
+""" lista1 = ["verde", "rojo", "amarillo"]
+lista2 = ["gris", "azul", "negro"]
+lista1.extend(lista2)
+print(lista1) """
+
+""" tupla = ("hola", "soy", "una", "tupla")
+print(tupla)
+print(tupla[0]) """
+
+""" lista = [1, 4, 892, 2, 983, 35]
+tupla = ("hola", "soy", "una", "tupla")
+print(len(lista))
+print(len(tupla)) """
+
+# Ejercicios
+
+""" lista_numeros = [10, 45, 55, 76]
+print(lista_numeros[3])
+print(f"El valor más pequeño en la lista es {lista_numeros[0]}. El más grande, el {lista_numeros[3]}.")
+
+lista = ["verde", "azul", "rojo", "amarillo"]
+print(lista[1][2])
+lista.insert(0, "gris")
+lista.append("rosa")
+lista.insert(3, "naranja")
+print(lista)
+lista.pop(1)
+lista.pop(3)
+lista.pop(-2)
+print(lista)
+
+
+lista = [4, 23, 542, 3, 532, 45, 23, 563, 2, 2, 6, 43, 64,32, 543, 4353, 354, 2, 64, 7, 43, 87, 25,7745, 2343]
+print(lista[-1])
+print(lista[-2])
+print(lista.index(3))
+lista.sort(reverse= True)
+print(lista)
+lista.sort()
+print(lista)
+print(len(lista))
+
+lista1 = [4, 4, 23, 542, 3, 532, 4]
+lista2 = lista1
+print(lista2)
+print(lista1.count(4)) """
+
+# No hay Proyecto
