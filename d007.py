4c8dba9e451ca93dca766ed497f73bb0ff5e1700
Samuel Lopez <samuellopezdemiguel586@gmail.com>
2022-09-24 10:04:54 +0200

    Dia siete completado

---
+++ d007.py
@@ -0,0 +1,63 @@
+""" diccionario = {
+    "nombre": "samuel",
+    "nombre1": "anchel",
+    "nombre2": "toni"
+}
+
+diccionario2 = {
+    1: "Hola",
+    2: "hola",
+    3: "ola"
+}
+
+diccionario2[4] = "alo"
+
+print(diccionario)
+print(diccionario["nombre1"])
+print(diccionario2[3])
+print(diccionario2[4]) """
+
+""" vacio = {
+    1: "Espada",
+    2: "Escudo",
+    3: "Armadura"
+}
+print(vacio)
+vacio = {}
+print(vacio) """
+
+""" documento = {
+    1: "Pepe"
+}
+documento[1] = "Samuel"
+print(documento[1]) """
+
+""" diccionario2 = {
+    1: "Hola",
+    2: "hola",
+    3: "ola"
+}
+
+for number in diccionario2:
+    print(f"{number}: {diccionario2[number]}") """
+
+""" set = {"perro", "gato", "periquito", "ballena", "perro"}
+print(set)
+print(set) """
+
+# Ejercicios:
+
+""" colores = {
+    1: "rojo",
+    2: "azul",
+    3: "verde", 
+    4: "amarillo"
+}
+
+print(colores)
+
+for color in colores:
+    print(f"{color}- {colores[color].capitalize()}")
+
+colores[5] = "rosa"
+print(colores) """
