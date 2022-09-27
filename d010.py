8de5a6ce6d0830a8bdf1979169226d00856c1a90
Samuel Lopez <samuellopezdemiguel586@gmail.com>
2022-09-27 08:59:46 +0200

    Dia diez completado

---
+++ d010.py
@@ -0,0 +1,32 @@
+# Funcion normal
+
+""" def funcion(num1, num2):
+    return num1 + num2 """
+
+# Funcion lambda
+
+""" suma = lambda num1, num2: num1 + num2
+resultado1 = suma(5,5)
+print(resultado1) """
+
+""" (lambda num1, num2: print(num1 + num2))(5,5) """
+
+# Ejercicios
+
+""" radio_cm = int(input("Introduzca el ára del círculo en cm.\n"))
+
+PI = 3.14159265359
+
+calcula_area = lambda radio : PI * radio * radio
+
+area = round(calcula_area(radio_cm),2)
+
+print(f"El área del círculo es de {area} cm.") """
+
+""" (lambda name : print(f"Hola {name}"))("Samuel") """
+
+""" colores = ["rojo", "azul", "verde", "amarillo"]
+
+(lambda color : print(f"El color se encuentra en la posicion {colores.index(color)} de la lista colores"))("rojo") """
+
+# No hay proyecto
